7eba8dd207837bde24ba223b5706041a60f4c276
Nicholas Baroni <nick@rhythnic.com>
2015-03-07 11:08:26 -0500

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp      = require('gulp'),
+    stylus    = require('gulp-stylus'),
+    connect = require('gulp-connect'),
+    nib       = require('nib'),
+    jeet      = require('jeet'),
+    axis      = require('axis'),
+    rupture   = require('rupture'),
+    maneuver  = require('maneuver');
+
+gulp.task('styl', function () {
+  gulp.src('./docs/docs.styl')
+    .pipe(stylus({ use: [nib(), jeet(), axis(), rupture(), maneuver()], compress: true }))
+    .pipe(gulp.dest('./docs/css'))
+    .pipe(connect.reload());
+});
+ 
+gulp.task('connect', function() {
+  connect.server({
+    root: '',
+    livereload: true
+  });
+});
+
+gulp.task('watch', function() {
+    gulp.watch('docs/*.styl', ['styl']);
+});
+
+gulp.task('default', ['connect', 'watch']);
