0a83fa68159dfd3b4ef4f4c467934bf6136c31aa
Nicholas Baroni <nick@rhythnic.com>
2015-03-22 00:29:06 -0400

    Added flipcard

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -5,3 +5,4 @@ $('.oc-1 button').on('click', function (e) { $('.oc-1').toggleClass('open'); });
 $('.oc-2 button').on('click', function (e) { $('.oc-2').toggleClass('open'); });
 $('.oc-3 .toggle-top').on('click', function (e) { $('.oc-3').toggleClass('open-top'); });
 $('.oc-3 .toggle-bottom').on('click', function (e) { $('.oc-3').toggleClass('open-bottom'); });
+$('.flipcard1').on('click', function (e) { $(e.currentTarget).toggleClass('flip'); });
