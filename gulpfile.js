17c6c5cd96f98d706bf11f8de4ab69cfd7cfa3cd
Nick Baroni <nick@rhythnic.com>
2015-05-25 22:43:26 -0400

    Restructured, mid-create new layout, adding tabs

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,31 @@
+function toText(filePath, file) {
+  return file.contents.toString('utf8');
+}
+
 var gulp      = require('gulp'),
     stylus    = require('gulp-stylus'),
     connect = require('gulp-connect'),
+    concat = require('gulp-concat'),
+    inject = require('gulp-inject'),
     nib       = require('nib'),
     jeet      = require('jeet'),
     axis      = require('axis'),
     rupture   = require('rupture'),
     maneuver  = require('maneuver');
 
+gulp.task('default', ['index', 'styl', 'script', 'connect', 'watch']);
+
 gulp.task('styl', function () {
-  gulp.src('./docs/docs.styl')
+  gulp.src('./src/**/*.styl')
     .pipe(stylus({ use: [nib(), jeet(), axis(), rupture(), maneuver()], compress: true }))
-    .pipe(gulp.dest('./docs/css'))
+    .pipe(concat('app.css'))
+    .pipe(gulp.dest('./public'))
+    .pipe(connect.reload());
+});
+
+gulp.task('script', function () {
+  gulp.src('./src/**/*.js')
+    .pipe(gulp.dest('./public'))
     .pipe(connect.reload());
 });
  
@@ -22,7 +37,50 @@ gulp.task('connect', function() {
 });
 
 gulp.task('watch', function() {
-    gulp.watch('docs/*.styl', ['styl']);
+    gulp.watch('./src/**/*.styl', ['styl']);
+    gulp.watch('./src/**/*.html', ['index']);
+    gulp.watch('./src/**/*.js', ['script']);
 });
 
-gulp.task('default', ['connect', 'watch']);
+gulp.task('index', function () {
+  gulp.src('./src/index.html')
+    .pipe(inject(gulp.src(['./src/partials/head.html']), {
+      starttag: '<!-- inject:head:{{ext}} -->',
+      transform: toText
+    }))
+    .pipe(inject(gulp.src(['./src/partials/sidebar/sidebar.html']), {
+      starttag: '<!-- inject:sidebar:{{ext}} -->',
+      transform: toText
+    }))
+    .pipe(inject(gulp.src(['./src/partials/header/header.html']), {
+      starttag: '<!-- inject:header:{{ext}} -->',
+      transform: toText
+    }))
+    .pipe(inject(gulp.src(['./src/partials/about/about.html']), {
+      starttag: '<!-- inject:about:{{ext}} -->',
+      transform: toText
+    }))
+    .pipe(inject(gulp.src(['./src/partials/mixins/offcanvas/offcanvas.html']), {
+      starttag: '<!-- inject:offcanvas:{{ext}} -->',
+      transform: toText
+    }))
+    .pipe(inject(gulp.src(['./src/partials/mixins/offcanvasMulti/offcanvasMulti.html']), {
+      starttag: '<!-- inject:offcanvasMulti:{{ext}} -->',
+      transform: toText
+    }))
+    .pipe(inject(gulp.src(['./src/partials/mixins/tabs/tabs.html']), {
+      starttag: '<!-- inject:tabs:{{ext}} -->',
+      transform: toText
+    }))
+    .pipe(inject(gulp.src(['./src/partials/mixins/flipCard/flipCard.html']), {
+      starttag: '<!-- inject:flipCard:{{ext}} -->',
+      transform: toText
+    }))
+    .pipe(inject(gulp.src(['./src/partials/mixins/toggle/toggle.html']), {
+      starttag: '<!-- inject:toggle:{{ext}} -->',
+      transform: toText
+    }))
+    .pipe(gulp.dest('./'))
+    .pipe(connect.reload());
+});
+
--- public/app.js
+++ public/app.js
@@ -6,3 +6,10 @@ $('.oc-2 button').on('click', function (e) { $('.oc-2').toggleClass('open'); });
 $('.oc-3 .toggle-top').on('click', function (e) { $('.oc-3').toggleClass('open-top'); });
 $('.oc-3 .toggle-bottom').on('click', function (e) { $('.oc-3').toggleClass('open-bottom'); });
 $('.flipcard1').on('click', function (e) { $(e.currentTarget).toggleClass('flip'); });
+
+$('.tabs > div:first-child').on('click', function (e) {
+	var index = $(e.target).index();
+	$(this).next().children().removeClass('active').eq(index).addClass('active');
+	$(this).children().removeClass('active').eq(index).addClass('active');
+});
+
